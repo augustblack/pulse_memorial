f763c16e83afe1628ec3766a9fbd4e3410a2275f
August Black <augustblack@gmail.com>
2025-05-13 17:50:27 -0600

    put back in padding on button

--- ui/src/app.tsx
+++ ui/src/app.tsx
@@ -138,7 +138,7 @@ function App() {
           }
         </button>
       </div >
-      <button class="absolute left-2 bottom-2 w-12 h-12 btn btn-ghost" onClick={openModal}>
+      <button class="absolute left-2 bottom-2 w-12 h-12 btn btn-ghost p-0" onClick={openModal}>
         <svg id="infosvg" xmlns="http://www.w3.org/2000/svg" class="w-full h-full" height="24px" viewBox="0 0 24 24" width="24px" fill="currentColor">
           <path d="M0 0h24v24H0V0z" fill="none"></path>
           <path d="M11 7h2v2h-2zm0 4h2v6h-2zm1-9C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm0 18c-4.41 0-8-3.59-8-8s3.59-8 8-8 8 3.59 8 8-3.59 8-8 8z"></path>
